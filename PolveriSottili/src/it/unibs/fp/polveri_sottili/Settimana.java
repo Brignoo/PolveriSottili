eb702100c2c7518a5be6d371d60e0706bfea3b9d
IlBuonTommy <tomamsobianchin23@gmail.com>
2021-05-13 15:56:29 +0200

    Aggiunta javadoc

--- PolveriSottili/src/it/unibs/fp/polveri_sottili/Settimana.java
+++ PolveriSottili/src/it/unibs/fp/polveri_sottili/Settimana.java
@@ -15,30 +15,58 @@ public class Settimana {
 		this.numeroSettimana = numeroSettimana;
 	}
 
+	
+	/** Get array valori
+	 * @return double[] Ritorna un'array dei valori
+	 */
 	public double[] getValori() {
 		return valori;
 	}
 
+	
+	/** Effetua un set dei vlaori
+	 * @param valori I valori presi in input verrano settati
+	 */
 	public void setValori(double[] valori) {
 		this.valori = valori;
 	}
 
+	
+	/** Get del numeroSettimana
+	 * @return int retrun del valore richiesto
+	 */
 	public int getNumeroSettimana() {
 		return numeroSettimana;
 	}
 
+	
+	/** set del numero settimana
+	 * @param numeroSettimana Parametro che verrà utilizzato per settare la variabile numeroSettimana
+	 */
 	public void setNumeroSettimana(int numeroSettimana) {
 		this.numeroSettimana = numeroSettimana;
 	}
 
+	
+	/** Get anno
+	 * @return int return dell'anno desiderato
+	 */
 	public int getAnno() {
 		return anno;
 	}
 
+	
+	/** Set anno
+	 * @param anno numero che verrà utilizzato come valore dell'anno
+	 */
 	public void setAnno(int anno) {
 		this.anno = anno;
 	}
 	
+	
+	/** Calcola il valore massimo di una settimana
+	 * @return double effettua il return del valore massimo della settimana
+	 */
 	public double getMassimo(){
 		double max=valori[0];
 		for (double d : valori) {
@@ -48,6 +76,10 @@ public class Settimana {
 		return max;
 	}
 
+	
+	/** Calcola la media di una specifica settimana
+	 * @return double Restituisce il valore della media delle misure di quella settimana
+	 */
 	public double calcolaMedia(){
 		double media=0;
 		for (double d : valori) {
@@ -56,6 +88,10 @@ public class Settimana {
 		return media/DIM;
 	}
 
+	
+	/** Effettua il to string dei valori di una settimana
+	 * @return String In questa String sono riportati i valori delle misurazioni di una settimana
+	 */
 	public String toStringValori(){
 		String temp= "\nValori: ";
 		for (double d : valori) {
@@ -64,10 +100,18 @@ public class Settimana {
 		return temp.substring(0, temp.length()-2)+".";
 	}
 	
+	
+	/** Effetua il get del DIM
+	 * @return int Return del parametro DIM
+	 */
 	public int getDIM(){
 		return DIM;
 	}
 
+	
+	/** Effettua il to string di questa classe
+	 * @return String All'interno di questa stinga vengono riportati tutti i parametri di una settimana
+	 */
 	public String toString(){
 		return "\nAnno: " + anno + "\t\t\tSettimana: " + numeroSettimana 
 		+ "\nMassimo giornaliero: " + String.format("%.2f", getMassimo()) + "\tMedia settimanale: " + String.format("%.2f", calcolaMedia()) + "\n";
