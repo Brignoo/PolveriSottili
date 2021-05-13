32a2024bd0e5903a9a34654b27d3394015c40244
IlBuonTommy <tomamsobianchin23@gmail.com>
2021-05-13 15:21:27 +0200

    String

--- PolveriSottili/src/it/unibs/fp/polveri_sottili/Settimana.java
+++ PolveriSottili/src/it/unibs/fp/polveri_sottili/Settimana.java
@@ -55,6 +55,22 @@ public class Settimana {
 		}
 		return media/DIM;
 	}
+
+	public int getDIM(){
+		return DIM;
+	}
+
+	public String toStringValori(){
+		String temp= "\nValori: ";
+		for (double d : valori) {
+			temp += d+", ";
+		}
+		return temp.substring(0, temp.length()-2)+".";
+	}
+
+	public String toString(){
+		return "Settimana: "+numeroSettimana+" anno: "+anno+"\nMassimo giornaliero: "+getMassimo()+"\nMedia settimanale: "+calcolaMedia()+toStringValori();
+	}
 	
 	public int getDIM(){
 		return DIM;
